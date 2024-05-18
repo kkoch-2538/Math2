f9fb8b2760158b59390fc7d5e5262ffaa8251c0f
kkoch-2538 <huacaijicun7@gmail.com>
2024-05-19 01:35:55 +0900

    Initial Commit

---
+++ Math2/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Math2
+//
+//  Created by 吉村花菜 on 2024/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    @IBOutlet var Button1: UIButton!
+    @IBOutlet var Button2: UIButton!
+    @IBOutlet var Button3: UIButton!
+    @IBOutlet var Button4: UIButton!
+    @IBOutlet var Button5: UIButton!
+    @IBOutlet var Button6: UIButton!
+    @IBOutlet var Button7: UIButton!
+    @IBOutlet var Button8: UIButton!
+    @IBOutlet var Button9: UIButton!
+    @IBOutlet var Button10: UIButton!
+    @IBOutlet var Button11: UIButton!
+    @IBOutlet var Button12: UIButton!
+    
+    
+    override func viewDidLoad() {
+        label.layer.cornerRadius = 10
+        label.clipsToBounds = true
+        super.viewDidLoad()
+    }
+   
+    
+}
+
